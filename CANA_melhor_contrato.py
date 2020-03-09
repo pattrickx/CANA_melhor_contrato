5531100c21345020f5b5426363b246c17ac0b253
Patrick Martins <pattrickpx99@gmail.com>
2020-03-09 09:05:08 -0300

    Pegando menor contrato, não comentado

--- CANA_melhor_contrato.py
+++ CANA_melhor_contrato.py
@@ -13,12 +13,12 @@ for i in range(len(dado)):
 
 x=[]
 for i in dado: # ajusta dados adequar a matriz tornando mes de 1a3 para de 0a1 
-    x.append([i[x]-1 if x>0 and x<len(i) else i[x] for x in range(0,len(i))]) 
+    x.append([i[x]-1 if x>0 and x<3 else i[x] for x in range(0,len(i))]) 
 
 dado = x # dado apos tratamentos
 meses_provedores=dado.pop(0) ## pega cabeçalho
 
-M_melhor = [cp.deepcopy([-1]*meses_provedores[0]) for i in range(meses_provedores[0])]#cria matriz de meses por meses
+M_melhor = [cp.deepcopy([0]*meses_provedores[0]) for i in range(meses_provedores[0])]#cria matriz de meses por meses
 M_provedores = cp.deepcopy(M_melhor)
 #M_provedores = cp.deepcopy(M_melhor) 
 #    ## ele cria uma copia do arquivo separando do indereço de memoria
@@ -39,4 +39,31 @@ for i in range(len(dado)):  ## os dois for percorem da diagonal principal da mat
 
 print(M_melhor)
 print(M_provedores)
-            
+soma = 0
+s_contratos=[]
+soma2 =0
+s2_contratos=[]
+for i in range(len(M_melhor)):
+    for j in range(i,len(M_melhor)):
+        soma2+=M_melhor[i][j]
+        s2_contratos.append([M_provedores[i][j],i,j])
+        for k in range(len(M_melhor)):
+            for l in range(k,len(M_melhor)):
+                if k==i or (l>=i and l<=j):
+                    break
+                soma2 += M_melhor[k][l]
+                s2_contratos.append([M_provedores[k][l],k,l])
+                break
+        print(soma2)
+        print(s2_contratos)
+        if soma>soma2 or soma==0:
+            soma=soma2
+            s_contratos.clear()
+            s_contratos=cp.deepcopy(s2_contratos)
+            soma2=0
+            s2_contratos.clear()
+        else:
+            soma2=0
+            s2_contratos.clear()
+print("contrato",s_contratos)
+print("preço",soma)
