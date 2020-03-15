daf933e23bd5c8f5117aac2e947abddff32598bd
Gabrielcarv97 <60945133+Gabrielcarv97@users.noreply.github.com>
2020-03-15 00:51:32 -0300

    Update CANA_melhor_contrato.py

--- CANA_melhor_contrato.py
+++ CANA_melhor_contrato.py
@@ -62,8 +62,8 @@ def menor_contrato(dado,meses,provedores): # Questão E e F
 def menor_contrato_1_M(fim,vendedor,dado,meses): # Questão G e H
     valor=dado[0][0][fim]                                                                #|       1       |        1      |
     contrato=[0,0,fim,valor]                                                             #|       1       |        1      |
-    for i in range(vendedor):                                                            #|       N       |        N      |
-        if valor>dado[i][0][fim]:                                                        #|       N+1     |        N+1    |
+    for i in range(vendedor):                                                            #|       N+1     |        N+1    |
+        if valor>dado[i][0][fim]:                                                        #|       N       |        N      |
             valor=dado[i][0][fim]                                                        #|       1       |        N      |  
             contrato=[i+1,1,fim+1,valor]                                                 #|       1       |        N      |
     return contrato                                                                      #|       1       |        1      |   
