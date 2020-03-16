2985aed940085c62ecb26a48bd3094de58cd129b
Patrick Martins <pattrickpx99@gmail.com>
2020-03-16 17:29:08 -0300

    Ajustes analise de complexidade

--- CANA_melhor_contrato.py
+++ CANA_melhor_contrato.py
@@ -1,17 +1,16 @@
 import copy as cp
 from datetime import datetime
 # entrada de dado
-def entrada(nome_do_arquivo): # Questão A e B                                           # | MELHOR CASO | PIOR CASO |
-    arquivo=open(nome_do_arquivo,'r') # recebe arquivo em modo de leitura                 |      1      |      1    |
-    meses_provedores=list(map(int,arquivo.readline().strip().split(" ")))               # |      1      |      1    |
-    dado_bruto=arquivo.readlines() # cria um array com todas as linas do arquivo        # |      1      |      1    |
+def entrada(nome_do_arquivo): # Questão A e B                                                    
+    arquivo=open(nome_do_arquivo,'r') # recebe arquivo em modo de leitura                          |      1       |
+    meses_provedores=list(map(int,arquivo.readline().strip().split(" "))) #Ler primeira linha      |      1       |
+    dado_bruto=arquivo.readlines() # cria um array com todas as linas do arquivo                 # |      1       |
     ## tratamento de dado
-  
-    dado=[cp.deepcopy([0]*meses_provedores[0]) for i in range(meses_provedores[0])]     # |      1      |     M     |
-    dado=[cp.deepcopy(dado)for i in range(meses_provedores[1])]                         # |    M + 1    |      M    |
-    for i in range(len(dado_bruto)):                                                    # |      M      |    M+1    |
-        x=dado_bruto[i].strip().split(" ")                                              # |      1      |     1     |
-        dado[int(x[0])-1][int(x[1])-1][int(x[2])-1]=float(x[3])                         # | (N-1)*2 + 1 |  (N^2)*2/2|
+    dado=[cp.deepcopy([0]*meses_provedores[0]) for i in range(meses_provedores[0])]              # |    N+1       |
+    dado=[cp.deepcopy(dado)for i in range(meses_provedores[1])]                                  # |    N+1       |
+    for i in range(len(dado_bruto)):                                                             # | M(N(N+1)/2)+1|
+        x=dado_bruto[i].strip().split(" ")                                                       # | M(N(N+1)/2)  |
+        dado[int(x[0])-1][int(x[1])-1][int(x[2])-1]=float(x[3])                                  # | M(N(N+1)/2)  |
 
     #dado[i]= [int(x) for x in (list(map(float,dado[i].strip().split(" "))))] # converte de float para int pois como ultimo valor é uma string representando float converter direto pra int não é possivel
     ## strip remove ultima posoção revendo o /n
@@ -23,14 +22,14 @@ def entrada(nome_do_arquivo): # Questão A e B                                  
                 #   ESTRUTURA USADA FOI UMA MATRIZ TRIDIMENCIONAL
                 #   SUA COMPLEXIDADE É DE   TETA (N*M^2)
 
-    return dado,meses_provedores # dado apos tratamentos                                # |       1      |     1      |
-                                                                                        # | F(N)=(M(N))  |F(N)=(M(N^2)| *notação assintotica
+    return dado,meses_provedores # dado apos tratamentos                                        # |       1      |
+                                                                                                # | F(N,M)=Teta(N*M^2)|
 
-                                                                                        # |F(N)=O(M(N^2)| *notação assintotica do metodo
+                                                                                              
 
 #-------------------------------------------------------------------------------------------------------------------------------------------------------
 
-def menor_contrato_vendedor(vendedor,dado,meses): # Questão C e D
+def menor_contrato_vendedor(vendedor,dado,meses): # Questão C e D                            Melhor         Pior
     valor=dado[vendedor][0][0]                                                          # |       1      |     1      |
     contrato=[vendedor,0,0,valor]                                                       # |       1      |     1      |
     for i in range(meses):                                                              # |      N+1     |    N+1     |
@@ -38,21 +37,21 @@ def menor_contrato_vendedor(vendedor,dado,meses): # Questão C e D
             valor=dado[vendedor][i][i]                                                  # |       0      |     N      |
             contrato=[vendedor+1,i+1,i+1,valor]                                         # |       0      |     N      |
     return contrato                                                                     # |       1      |     1      |
-                                                                                        # |   F(N)=O(N)    |   F(N)=O(N)   |*notação assintotica
+                                                                                        # |F(N)=Teta(N)  |F(N)=Teta(N)|*notação assintotica
 
-                                                                                        # |   F(N)=O(N)  |  *notação assintotica do metodo
+                                                                                        # |   F(N)=Teta(N)  |  *notação assintotica do metodo
 
 #-------------------------------------------------------------------------------------------------------------------------------------------------------
 def menor_contrato(dado,meses,provedores): # Questão E e F
     valor=dado[0][0][0]                                                                 #|        1      |        1      |
     contrato=[0,0,valor]                                                                #|        1      |        1      |
-    for i in range(provedores):                                                         #|        N      |        N      |
-        for j in range(meses):                                                          #|       N*M     |       N*M     |
-            if valor>dado[i][j][j]:                                                     #|       N*M     |       N*M     |
-                valor=dado[i][j][j]                                                     #|        0      |       N*M     |
-                contrato=[i+1,j+1,j+1,valor]                                            #|        0      |       N*M     |
+    for i in range(provedores):                                                         #|       M+1     |       M+1     |
+        for j in range(meses):                                                          #|       M*N+1   |       M*N+1   |
+            if valor>dado[i][j][j]:                                                     #|       M*N     |       M*N     |
+                valor=dado[i][j][j]                                                     #|        0      |       M*N     |
+                contrato=[i+1,j+1,j+1,valor]                                            #|        0      |       M*N     |
     return contrato                                                                     #|        1      |       1       |
-                                                                                        #| F(N,M)=O(M*N)   |  F(N,M)=O(M*N)| *notação assintotica
+                                                                                        #|F(N,M)=Teta(M*N)|F(N,M)=Teta(M*N)| *notação assintotica
 
                                                                                        # | F(N,M)=O(M*N)|  *notação assintotica do metodo
 
@@ -62,39 +61,39 @@ def menor_contrato(dado,meses,provedores): # Questão E e F
 def menor_contrato_1_M(fim,vendedor,dado,meses): # Questão G e H
     valor=dado[0][0][fim]                                                                #|       1       |        1      |
     contrato=[0,0,fim,valor]                                                             #|       1       |        1      |
-    for i in range(vendedor):                                                            #|       N+1     |        N+1    |
-        if valor>dado[i][0][fim]:                                                        #|       N       |        N      |
-            valor=dado[i][0][fim]                                                        #|       1       |        N      |  
-            contrato=[i+1,1,fim+1,valor]                                                 #|       1       |        N      |
+    for i in range(vendedor):                                                            #|       M+1     |        M+1    |
+        if valor>dado[i][0][fim]:                                                        #|       M       |        M      |
+            valor=dado[i][0][fim]                                                        #|       1       |        M      |  
+            contrato=[i+1,1,fim+1,valor]                                                 #|       1       |        M      |
     return contrato                                                                      #|       1       |        1      |   
-                                                                                         #|   F(N)=O(N)   |     F(N)=O(N) | *notação assintotica
+                                                                                         #|   F(M)=Teta(M)|F(M)=Teta(M) | *notação assintotica
     
-                                                                                         #| F(N)=O(N)|  *notação assintotica do metodo
+                                                                                         #| F(M)=Teta(M)|  *notação assintotica do metodo
 
-#------------------------------------------[I,J]----------------------------------------------------")
+#------------------------------------------[I,J]----------------------------------------------------"
 #-------------------------------------------------------------------------------------------------------------------------------------------------------
 def melhores(meses,provedores,dado):
-    M_melhor = [cp.deepcopy([0]*meses) for i in range(meses)]                          #|        N       |     N      |       #cria matriz de meses por meses
-    M_provedores = cp.deepcopy(M_melhor)                                               #|        1       |      1     |
+    M_melhor = [cp.deepcopy([0]*meses) for i in range(meses)]              #|        N       |     N      |       #cria matriz de meses por meses
+    M_provedores = cp.deepcopy(M_melhor)                                   #|        1       |      1     |
     #M_provedores = cp.deepcopy(M_melhor)
-    #    ## ele cria uma copia do arquivo separando do indereço de memoria
-    #       inicial do copiado , caso não seja usado as duas variaveia usaram o mesmo endereço de moria prejudicando
-    #        o funcionamento do codigo
+    # ele cria uma copia do arquivo separando do indereço de memoria
+    # inicial do copiado , caso não seja usado as duas variaveia usaram o mesmo endereço de moria prejudicando
+    #  o funcionamento do codigo
     
 
-    for i in range(meses):                                                             #|        N        |    N       |
-        for j in  range(i,meses):                                                      #|   [(N+1)]*N/2   | [(N+1)]*N/2     |
-            valor=0                                                                    #|   [(N+1)]*N/2   | [(N+1)]*N/2     |
-            for k in range(provedores):                                                #|[[(N+1)]*N/2]*M  |[[(N+1)]*N/2]*M  |
-                if valor==0 or valor>dado[k][i][j]:                                    #|[[(N+1)]*N/2]*M  |[[(N+1)]*N/2]*M  |
-                    valor=dado[k][i][j]                                                #|        0        |[[(N+1)]*N/2]*M  |
-                    M_melhor[i][j]=valor                                               #|        0        |[[(N+1)]*N/2]*M  |
-                    M_provedores[i][j]=k                                               #|        0        |[[(N+1)]*N/2]*M  |
+    for i in range(meses):                                                 #|        N+1      |    N+1          |
+        for j in  range(i,meses):                                          #|   [(N+1)]*N/2   | [(N+1)]*N/2     |
+            valor=0                                                        #|   [(N+1)]*N/2   | [(N+1)]*N/2     |
+            for k in range(provedores):                                    #|[[(N+1)]*N/2]*(M+1)|[[(N+1)]*N/2]*(M+1)  |
+                if valor==0 or valor>dado[k][i][j]:                        #|[[(N+1)]*N/2]*M  |[[(N+1)]*N/2]*M  |
+                    valor=dado[k][i][j]                                    #|        1        |[[(N+1)]*N/2]*M  |
+                    M_melhor[i][j]=valor                                   #|        1        |[[(N+1)]*N/2]*M  |
+                    M_provedores[i][j]=k                                   #|        1        |[[(N+1)]*N/2]*M  |
 
-    return M_melhor,M_provedores                                                       #|        1        |      M     |
-                                                                                       #|  F(N,M)=O((N^2)*M)   | F(N,M)=O((N^2)*M) *notação assintotica
+    return M_melhor,M_provedores                                           #|        1        |      1          |
+                                                                           #| F(N,M)=Teta((N^2)*M)| F(N,M)=Teta((N^2)*M) *notação assintotica
 
-                                                                                       #| F(N,M)=O((N^2)*M)   *notação assintotica do Metodo
+                                                                           #| F(N,M)=Teta((N^2)*M)   *notação assintotica do Metodo
 
 
 #-------------------------------------------------------------------------------------------------------------------------------------------------------
@@ -109,14 +108,14 @@ def menor(meses,M_melhor,M_provedores):
     #meses=5
     soma=0
     n=0
-    for i in range(meses):                                                                #|     M       |      M       |
-        soma2=M_melhor[0][i]                                                              #|     M       |      M       |
-        s2_contratos.append([M_provedores[0][i]+1,1,i+1,soma2])                           #|     M       |      M       |
-        for j in range(i+1 ,meses):                                                       #|     M+1     |  [(M+1)]*M/2 |
+    for i in range(meses):                                                                #|     N       |      N       |
+        soma2=M_melhor[0][i]                                                              #|     N       |      N       |
+        s2_contratos.append([M_provedores[0][i]+1,1,i+1,soma2])                           #|     N       |      N       |
+        for j in range(i+1 ,meses):                                                       #|     N+1     |  [(M+1)]*M/2 |
                # recebe o primeiro valor do teste 
             #for k in range(j,meses):
-            soma2 += M_melhor[j][j]                                                       #|     M+1     |  [(M+1)]*M/2 |
-            s2_contratos.append([M_provedores[j][j]+1,j+1,j+1,M_melhor[j][j]])            #|     M+1     |  [(M+1)]*M/2 |
+            soma2 += M_melhor[j][j]                                                       #|     N+1     |  [(M+1)]*M/2 |
+            s2_contratos.append([M_provedores[j][j]+1,j+1,j+1,M_melhor[j][j]])            #|     N+1     |  [(M+1)]*M/2 |
                 #break
                 
         if soma>soma2 or soma==0:                                                         #|     1       |  [(M+1)]*M/2 |
@@ -129,11 +128,11 @@ def menor(meses,M_melhor,M_provedores):
             soma2=0                                                                       #|     0       |      0       |
             s2_contratos.clear()                                                          #|     0       |      0       |
     
-    return s_contratos,soma                                                               #|       1     |     1        |
+    return s_contratos,soma                                                               #|      1     |     1        |
 
-                                                                                          #| F(N,M)=O(M) | F(M)=O(M^2)  | *notação assintotica
+                                                                                          #| F(N)=Teta(N) | F(N)=Teta(N^2)  | *notação assintotica
 
-                                                                                          #| F(M)=O(M^2) |   *notação assintotica do Metodo
+                                                                                          #| F(N)=Teta(N^2) |   *notação assintotica do Metodo
 
 
 
