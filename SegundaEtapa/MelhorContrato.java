459c1bda649cae4c990a0e4c7f4b469904f5da7c
Patrick Martins <pattrickpx99@gmail.com>
2020-05-07 10:07:38 -0300

    MergeSort finish and upgrade DAO and main

--- SegundaEtapa/MelhorContrato.java
+++ SegundaEtapa/MelhorContrato.java
@@ -6,18 +6,22 @@ import Sort.DAO;
 import Sort.Contrato;
 import java.util.ArrayList;
 public class MelhorContrato {
-    
     public static void main(String[] args) {
 
-        DAO tratamento= new DAO("entrada.txt");
-        ArrayList<Contrato> lContratos= tratamento.getLista();
-        float[][][] MContratos= tratamento.CreateGetMatrix();
+        DAO tratamento = new DAO("entrada.txt");
+        ArrayList<Contrato> lContratos = tratamento.getLista();
+        float[][][] MContratos = tratamento.CreateGetMatrix();
         int head[] = tratamento.head();
-        System.out.println("Quantidade de meses: "+head[0]);
-        System.out.println("Quantidade de Fornecedores: "+head[1]);
-        //MergeSort mergesort = new MergeSort();
+        System.out.println("######## Informações da entrada ########");        
+        System.out.println("Quantidade de meses: " + head[0]);
+        System.out.println("Quantidade de Fornecedores: " + head[1]);
+        tratamento.variablesInformation();
 
-        //mergesort.run();
+        /// 
+        MergeSort mergesort = new MergeSort();
+        mergesort.run(lContratos, 0, lContratos.size()-1);
+        System.out.println("Tempo de execução MergeSort: "+mergesort.get_execution_time()+" ms");
+           
         
     }
 }
