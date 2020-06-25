ce4080cb2253be60c65da9c1f0e18efdf1b5936e
Patrick Martins <pattrickpx99@gmail.com>
2020-06-25 00:10:27 -0300

    dijkstra implementação completa

---
+++ SegundaEtapa/NewAlgoritmos/aresta.java
@@ -0,0 +1,12 @@
+package NewAlgoritmos;
+
+public class aresta {
+    public float peso;
+    public int destino;
+    public int fornecedor;
+    public aresta(float peso,int destino,int fornecedor){
+        this.peso=peso;
+        this.destino=destino;
+        this.fornecedor=fornecedor;
+    }
+}
---
+++ SegundaEtapa/NewAlgoritmos/grafo.java
@@ -0,0 +1,13 @@
+package NewAlgoritmos;
+import java.util.ArrayList;
+
+import NewAlgoritmos.aresta;
+
+public class grafo {
+   public static int length;
+public ArrayList<aresta> arestas;
+   public grafo(){
+      arestas=new ArrayList<>();
+   }
+
+}
